1fbb6698cc1377fafc9e1a759de11a54f4e63562
maryfranks <maryfranks@gmail.com>
2017-10-03 17:23:13 -0400

    create results hash with products and matching listings

--- matches.rb
+++ matches.rb
@@ -1,4 +1,5 @@
 require 'jsonl'
+require 'pry'
 
 # make json objects into arrays of ruby hashes
 listings_file = File.read('data/listings.txt')
@@ -7,18 +8,40 @@ products_file = File.read('data/products.txt')
 products = JSONL.parse(products_file)
 puts "listings: #{listings.count}"
 puts "products: #{products.count}"
+
 # set up results array
+results = []
 
 # find matches and add them to results array
-  # loop over each product
-  # create a hash for each product with key product_name and key listings as empty array
-  # compare each product to each listing (another loop)
-    # check for manufacturer match
-    # if manufacturer matches, check for if product lists family
-      # if product lists family then check for family matches
+
+# loop over each product
+products.each do | product |
+# create a hash for each product with key product_name and key listings as empty array
+  product_hash = {}
+  product_hash[:product_name] = product["product_name"]
+  product_hash[:listings] = []
+# compare each product to each listing (another loop)
+  listings.each do | listing |
+  # check for manufacturer match
+    if listing["manufacturer"].downcase.include?(product["manufacturer"].downcase)
+      # if manufacturer matches, check for if product lists family
+      if product["family"] && listing["title"].downcase.include?(product["family"].downcase)
         # if family match then check model number matches
-        # if model number matches, find product in results and add listing to listings
-    # if no family, check for model number matches
-      # if model number matches, find product in results and add listing to listings
+        if listing["title"].downcase.include?(" #{product["model"].downcase} ")
+          # if model number matches, find product in results and add listing to listings
+          product_hash[:listings] << listing
+        end
+      # if no family, check for model number matches
+      elsif !product["family"]
+        if listing["title"].downcase.include?(" #{product["model"].downcase} ")
+          # if model number matches, find product in results and add listing to listings
+          product_hash[:listings] << listing
+        end
+      end
+    end
+  end
+  results << product_hash
+end
+puts results[0]
 
 # convert results to json and write to file
