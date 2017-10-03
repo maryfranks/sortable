66950f1af5106abeeb8bd95808fde8f7e0f2c8e3
maryfranks <maryfranks@gmail.com>
2017-10-03 16:58:32 -0400

    outline problem in matches.rb

---
+++ matches.rb
@@ -0,0 +1,17 @@
+# make json objects into arrays of ruby hashes
+
+# set up results array
+
+# find matches and add them to results array
+  # loop over each product
+  # create a hash for each product with key product_name and key listings as empty array
+  # compare each product to each listing (another loop)
+    # check for manufacturer match
+    # if manufacturer matches, check for if product lists family
+      # if product lists family then check for family matches
+        # if family match then check model number matches
+        # if model number matches, find product in results and add listing to listings
+    # if no family, check for model number matches
+      # if model number matches, find product in results and add listing to listings
+
+# convert results to json and write to file
